ab0cb1500c32d51f6fb2d4c8d59e249f5df48711
Robert Zenz <Bobby@bonsaimind.org>
2011-05-28 21:27:51 +0200

    Step 2: Lighting

--- Java3D_DevTest/src/java3d_devtest/Main.java
+++ Java3D_DevTest/src/java3d_devtest/Main.java
@@ -8,9 +8,15 @@
 
 package java3d_devtest;
 
-import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.geometry.Sphere;
 import com.sun.j3d.utils.universe.SimpleUniverse;
+import java.awt.Color;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.DirectionalLight;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
 /**
  *
@@ -22,12 +28,32 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+		// The universe...no, the answer is ot 42!
         SimpleUniverse universe = new SimpleUniverse();
+
+		// A group to hold objects.
 		BranchGroup group = new BranchGroup();
 
-		group.addChild(new ColorCube(0.3));
+		// We're adding a sphere.
+		group.addChild(new Sphere(0.5f));
 
+		// The light source
+		DirectionalLight blueLight = new DirectionalLight(new Color3f(Color.BLUE), new Vector3f(4.0f, -7.0f, -12.0f));
+
+		// The bounds for the light source.
+		BoundingSphere bounds = new BoundingSphere(new Point3d(0, 0, 0), 100);
+
+		// Make the light source a spherical one.
+		blueLight.setInfluencingBounds(bounds);
+
+		// Add the light source.
+		group.addChild(blueLight);
+
+		// Set the viewpoint to...something...
 		universe.getViewingPlatform().setNominalViewingTransform();
+
+		// Add the group to the universe.
 		universe.addBranchGraph(group);
     }
 
