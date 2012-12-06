9f944bbb5e75a2648fa04f9c2300db8b5b6a2881
Robert Zenz <Bobby@bonsaimind.org>
2012-12-06 23:06:40 +0100

    Does not work...I don't see right now why...

---
+++ NoiseTests/Simplex/src/simplex/Helper.java
@@ -0,0 +1,44 @@
+/*
+ * Public Domain
+ */
+package simplex;
+
+/**
+ *
+ * @author robert
+ */
+public final class Helper {
+
+	/**
+	 *
+	 * @param a First value
+	 * @param b Second value
+	 * @param offseet 0 = a, 1 = b
+	 * @return
+	 */
+	public static double interpolate(double a, double b, double offset) {
+		return a * (1 - offset) + b * offset;
+	}
+
+	public static double cosineInterpolate(double a, double b, double offset) {
+		double temp = (1 - Math.cos(offset * Math.PI)) * 0.5;
+		return a * (1 - temp) + b * temp;
+	}
+
+	/**
+	 *
+	 * @param a First Value
+	 * @param b First Target
+	 * @param m Second Target
+	 * @param n Second Value
+	 * @param offset
+	 * @return
+	 */
+	public static double cubicInterpolate(double a, double b, double m, double n, double offset) {
+		double p = (n - m) - (a - b);
+		double q = (a - b) - p;
+		double r = m - a;
+
+		return p * Math.pow(offset, 3) + q * Math.pow(offset, 2) + (r * offset) + b;
+	}
+}
--- NoiseTests/Simplex/src/simplex/NoiseCanvas.java
+++ NoiseTests/Simplex/src/simplex/NoiseCanvas.java
@@ -1,3 +1,6 @@
+/*
+ * Public Domain
+ */
 package simplex;
 
 import java.awt.Canvas;
@@ -10,7 +13,7 @@ import java.awt.Graphics;
  */
 public class NoiseCanvas extends Canvas {
 
-	private int stepWidth = 5;
+	private int stepWidth = 10;
 
 	public int getStepWidth() {
 		return stepWidth;
@@ -24,11 +27,36 @@ public class NoiseCanvas extends Canvas {
 	public void paint(Graphics g) {
 		super.paint(g);
 
-		for (int x = 0; x < getWidth(); x+=stepWidth) {
-			for (int y = 0; y < getHeight(); y+=stepWidth) {
-				int value = (int) (SimplexNoise.noise(x, y) * 128 + 128);
-				g.setColor(new Color(value, value, value));
-				g.fillRect(x, y, stepWidth, stepWidth);
+		int stepsWidth = (int) (getWidth() / stepWidth);
+		int stepsHeight = (int) (getHeight() / stepWidth);
+
+		int valueCount = stepsWidth * stepsHeight;
+		Double[] values = new Double[valueCount];
+
+		for (int x = 0; x < stepsWidth; x++) {
+			for (int y = 0; y < stepsHeight; y++) {
+				values[y * stepsWidth + x] = SimplexNoise.noise(x, y);
+			}
+		}
+
+		for (int x = 0; x < stepsWidth; x++) {
+			for (int y = 0; y < stepsHeight; y++) {
+				double topLeft = values[y * stepsWidth + x];
+				double topRight = values[y * stepsWidth + (x + 1)];
+				double bottomLeft = values[(y + 1) * stepsWidth + x];
+				double bottomRight = values[(y + 1) * stepsWidth + (x + 1)];
+
+				for (int detailX = 0; detailX < stepsWidth; detailX++) {
+					double currentLeft = Helper.cosineInterpolate(topLeft, bottomLeft, detailX / stepsWidth);
+					double currentRight = Helper.cosineInterpolate(topRight, bottomRight, detailX / stepsWidth);
+
+					for (int detailY = 0; detailY < stepsHeight; detailY++) {
+						double current = Helper.cosineInterpolate(currentLeft, currentRight, detailY / stepsHeight);
+						int value = (int) (current * 128 + 128);
+						g.setColor(new Color(value, value, value));
+						g.fillRect(x * stepsWidth + detailX, y * stepsHeight + detailY, 1, 1);
+					}
+				}
 			}
 		}
 	}
